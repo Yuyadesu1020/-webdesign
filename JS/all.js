cf12e85af59a30ca4a736fd47d78af1b57c16b29
Yuyadesu1020 <Puchiyongzai@gmail.com>
2025-01-30 23:58:52 +0900

    lightbox編集

--- JS/all.js
+++ JS/all.js
@@ -72,3 +72,15 @@ function zoomImage(img) {
     'wrapAround': true,//グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
     'albumLabel': ' %1 / total %2 '//合計枚数中現在何枚目かというキャプションの見せ方を変更できる
   })
+  
+  document.addEventListener("lightbox:opened", function() {
+    setTimeout(function() {
+        let imageContainer = document.querySelector(".lb-outerContainer");
+        if (imageContainer) {
+            imageContainer.style.position = "fixed";
+            imageContainer.style.top = "50%";
+            imageContainer.style.left = "50%";
+            imageContainer.style.transform = "translate(-50%, -50%)";
+        }
+    }, 100);
+});
